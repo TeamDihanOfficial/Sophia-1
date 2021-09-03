4009d160fe7360348dea216596b7c1b2d6cddbc9
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-03 09:08:04 +0530

    Update musicplayer.py

--- Sophia/modules/musicplayer.py
+++ Sophia/modules/musicplayer.py
@@ -1,26 +1,28 @@
-__mod_name__ = "⚡️Music-Play⚡️"
+__mod_name__ = "Music Player"
 
 __help__ = """
 =>> *Song Playing* 🎧 
-❍ /play <song name> - play song you requested
-❍ /dplay <song name> - play song you requested via deezer
-❍ /splay <song name> - play song you requested via jio saavn
-❍ /playlist - Show now playing list
-❍ /current - Show now playing
-❍ /song <song name> - download songs you want quickly
-❍ /search <query> - search videos on youtube with details
-❍ /deezer <song name> - download songs you want quickly via deezer
-❍ /saavn <song name> - download songs you want quickly via saavn
-❍ /video <song name> - download videos you want quickly
+- /play <song name> - play song you requested
+- /dplay <song name> - play song you requested via deezer
+- /splay <song name> - play song you requested via jio saavn
+- /playlist - Show now playing list
+- /current - Show now playing
+- /song <song name> - download songs you want quickly
+- /search <query> - search videos on youtube with details
+- /deezer <song name> - download songs you want quickly via deezer
+- /saavn <song name> - download songs you want quickly via saavn
+- /video <song name> - download videos you want quickly
+
 =>> *Admins only*
-❍ /player - open music player settings panel
-❍ /pause - pause song play
-❍ /resume - resume song play
-❍ /skip - play next song
-❍ /end - stop music play
-❍ /userbotjoin - invite assistant to your chat
-❍ /reload - Refresh admin list
+- /player - open music player settings panel
+- /pause - pause song play
+- /resume - resume song play
+- /skip - play next song
+- /end - stop music play
+- /userbotjoin - invite assistant to your chat
+- /reload - Refresh admin list
+
 =>>   *Use*
-1) Add @HexzyBot in your group to admin with all permission
-2) Add Assistant I'd to your group @HexzyAssistant
+1) Add @SophiaSLBot in your group to admin with all permission
+2) Add Assistant I'd to your group @DihanOfficialAssistant
 """
