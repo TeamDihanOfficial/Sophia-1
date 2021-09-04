46630259433a9a7a69ef8068f42d93d427a89384
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-04 10:36:51 +0530

    Update remote_cmds.py

--- Sophia/modules/remote_cmds.py
+++ Sophia/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from HexzyBot import dispatcher
-from HexzyBot.modules.helper_funcs.chat_status import (
+from Sophia import dispatcher
+from Sophia.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from HexzyBot.modules.helper_funcs.extraction import extract_user_and_text
-from HexzyBot.modules.helper_funcs.filters import CustomFilters
+from Sophia.modules.helper_funcs.extraction import extract_user_and_text
+from Sophia.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
