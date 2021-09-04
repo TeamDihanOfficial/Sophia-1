094f34a1a0be8910beb6dcd3f4de2acb3857cc57
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-04 10:47:31 +0530

    Update songdl.py

--- Sophia/modules/songdl.py
+++ Sophia/modules/songdl.py
@@ -12,9 +12,9 @@ from tswift import Song
 from youtube_dl import YoutubeDL
 from youtubesearchpython import SearchVideos
 
-from HexzyBot.conf import get_str_key
-from HexzyBot.pyrogramee.pluginshelper import get_text, progress
-from HexzyBot import pbot
+from Sophia.conf import get_str_key
+from Sophia.pyrogramee.pluginshelper import get_text, progress
+from Sophia import pbot
 
 GENIUS = get_str_key("GENIUS_API_TOKEN", None)
 
