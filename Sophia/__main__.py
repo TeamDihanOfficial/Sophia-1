04b1d09c72222b3b4c9c3518a02487f1f10a89a7
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-10 20:48:44 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -216,10 +216,7 @@ def start(update: Update, context: CallbackContext):
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
 
         else:
-            update.effective_message.reply_sticker(
-                random.choice(STICKERS),
-                timeout=60,
-            )
+            update.effective_message.reply_sticker(STICKERS),
             update.effective_message.reply_text(
                 PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(buttons),
